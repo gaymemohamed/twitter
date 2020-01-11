4a30f957a63a42634ba2f2eb09ff99e3340fda7c
new name <gaymemohamed@gmail.com>
2020-01-11 22:53:44 +0200

    signup updates

--- API/controllers/user-controller.js
+++ API/controllers/user-controller.js
@@ -4,31 +4,39 @@ const User = require('../models/user-model');
 
 
 // SignUp
-exports.user_signup = async (req , res , next)=>{
-    try{
-        let checkMail = await User.findOne({email : req.body.email});
-        if(checkMail){
+exports.user_signup = async (req, res, next) => {
+    try {
+        let checkMail = await User.findOne({ email: req.body.email });
+        if (checkMail) {
             return res.status(409).json({
-                message : "Email is Exist"
+                message: "Email is Exist"
             });
         }
-            else{
-                const user = new User ({
-                    name : req.body.name,
-                    phone : req.body.phone,
-                    email : req.body.email,
-                    password : req.body.password,
-                    img : req.body.img,
+        else {
+            const user = new User({
+                name: req.body.name,
+                phone: req.body.phone,
+                email: req.body.email,
+                password: req.body.password,
+                img: req.body.img,
+            })
+            let createUser = await User.create(user);
+            return res.status(201).json({
+                User: {
+                         id: createUser.id,
+                        name: createUser.name,
+                        phone: createUser.phone,
+                        email: createUser.email,
+                        img: createUser.img
+                    }
                 })
-                let createUser = await User.create(user);
-                return res.status(201).json({
-                    message : "User Created Successfully",
-                    userDetails : createUser
-                }); 
-            }
+            
+        }
 
-        } 
-    catch(err){
+    }
+    catch (err) {
         next(err);
     }
 };
+
+// login
